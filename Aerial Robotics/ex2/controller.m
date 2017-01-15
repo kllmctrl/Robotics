55eda78f3c93af5fb7f344a54413d11aa2937d60
kllmctrl <kllmctrl@gmail.com>
2017-01-15 10:06:59 +0800

    ex

--- Aerial Robotics/ex2/controller.m
+++ Aerial Robotics/ex2/controller.m
@@ -14,10 +14,37 @@ function [ u1, u2 ] = controller(~, state, des_state, params)
 %   Using these current and desired states, you have to compute the desired
 %   controls
 
-u1 = 0;
-u2 = 0;
+% u1 = 0;
+% u2 = 0;
 
 % FILL IN YOUR CODE HERE
 
-end
+% Proportional and Derivative Gains
+Kp_z = 80;
+Kd_z = 20;
 
+Kp_y = 20;
+Kd_y = 5;
+
+Kp_a = 1000;
+Kd_a = 10;
+
+u1 = params.mass*(params.gravity + des_state.acc(2,1) + Kd_z*(des_state.vel(2,1)-state.vel(2,1)) + Kp_z*(des_state.pos(2,1)-state.pos(2,1)));
+
+phi_c = -(1/params.gravity)*(des_state.acc(1,1) + Kd_y*(des_state.vel(1,1)-state.vel(1,1) + Kp_y*(des_state.pos(1,1)-state.pos(1,1))));
+phi_c_dot = 0;
+phi_c_2dot = 0;
+
+u2 = params.Ixx*(phi_c_2dot + Kd_a*(phi_c_dot-state.omega) + Kp_a*(phi_c-state.rot));
+%u2 = (Kd_a*(phi_c_dot-state.omega) + Kp_a*(phi_c-state.rot));
+%u2 = (phi_c_2dot + Kd_a*(phi_c_dot-state.omega) + Kp_a*(phi_c-state.rot));
+
+% Hover Controller
+% u1 = params.mass*(params.gravity + Kd_z*(-state.vel(2,1)) + Kp_z*(des_state.pos(2,1)-state.pos(2,1)));
+% 
+% phi_c = -(1/params.gravity)*(Kd_y*(-state.vel(1,1) + Kp_y*(des_state.pos(1,1)-state.pos(1,1))));
+% phi_c_dot   = 0;
+% phi_c_2dot  = 0;
+% 
+% u2 = params.Ixx*(phi_c_2dot + Kd_a*(phi_c_dot-state.omega) + Kp_a*(phi_c-state.rot));
+end
